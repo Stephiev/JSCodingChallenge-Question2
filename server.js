6599ddd52c2e7833148053a1e79266b89f32c7be
Stephanie Vasquez-Soltero <StephieVictoria@gmail.com>
2015-03-24 03:10:17 -0700

    Some clean up.

--- server.js
+++ server.js
@@ -6,26 +6,6 @@ var port = process.env.PORT || 3000;
 var server = http.createServer(function(req, res) {
 
   var path = url.parse(req.url).path
-  // console.log(path)
-
-   // var query = url.parse(req.url,true).query;
-   //      pic = query.image;
-   //      console.log(query)
-   //      console.log(pic)
-    // Responds to a root-url request with an index.html file
-
-    //read the image using fs and send the image content back in the response
-    // fs.readFile("/img/scores.png", function (err, content) {
-    //     if (err) {
-    //         res.writeHead(400, {'Content-type':'text/html'})
-    //         console.log(err);
-    //         res.end("No such image");
-    //     } else {
-    //         //specify the content type in the response will be an image
-    //         res.writeHead(200,{'Content-type':'image/jpg'});
-    //         res.end(content);
-    //     }
-    // });
 
 if (path === "/") {
 
